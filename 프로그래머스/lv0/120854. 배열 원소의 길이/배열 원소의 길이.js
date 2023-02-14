f587adec212f3999a4ace3560df60a2201587b4d
Inseo Yang <124547871+1nxeo@users.noreply.github.com>
2023-02-14 22:21:38 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.js
+++ 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.js
@@ -1,8 +1,13 @@
-function solution(strlist) {
-    let answer = [];
-    for (i=0;i<strlist.length;i++){
-       strlist[i] = strlist[i].length
-    }
-    answer = strlist
-    return answer;
+// function solution(strlist) {
+//     let answer = [];
+//     for (i=0;i<strlist.length;i++){
+//        strlist[i] = strlist[i].length
+//     }
+//     answer = strlist
+//     return answer;
+// }
+
+function solution(strlist){
+    return strlist.map(zzz => zzz.length)
+    
 }
