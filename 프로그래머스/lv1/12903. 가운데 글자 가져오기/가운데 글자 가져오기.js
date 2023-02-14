7c752a350d2d397c41cd5edd3288c8952b568959
Inseo Yang <124547871+1nxeo@users.noreply.github.com>
2023-02-14 23:50:55 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -0,0 +1,11 @@
+function solution(s) {
+    let answer = '';
+    for (i=0;i<s.length;i++){
+        if (s.length%2 == 0){
+            answer = s[(s.length/2)-1]+s[s.length/2]
+        }else{
+            answer = s[parseInt(s.length/2)]
+        }
+    }
+    return answer;
+}
