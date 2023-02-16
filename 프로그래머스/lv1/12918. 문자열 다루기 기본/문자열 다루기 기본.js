4223acde72e05509e726d12642bd1fd14bc03f14
Inseo Yang <124547871+1nxeo@users.noreply.github.com>
2023-02-16 20:32:38 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,8 +1,17 @@
-function solution(s) {
-    let answer = false;
-    return (s.length == 4 || s.length == 6) && (/^\d+$/.test(s))
-
-        
+// function solution(s) {
+//     let answer = false;
+//     return (s.length == 4 || s.length == 6) && (/^\d+$/.test(s))
     
-    return answer;
+//     return answer;
+// }
+
+
+function solution(s) {
+
+    if(s.length!==4 && s.length !==6) return false
+    
+    for(let i=0; i<s.length;i++){
+      if(isNaN(Number(s[i]))) return false
+    }
+    return true;
 }
