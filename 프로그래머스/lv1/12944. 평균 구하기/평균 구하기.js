2db029a30e781166a6eaa763c56ba48c80bec8e0
Inseo Yang <124547871+1nxeo@users.noreply.github.com>
2023-02-16 23:26:44 +0900

    [level 1] Title: 평균 구하기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.js
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.js
@@ -1,7 +1,7 @@
 function solution(arr) {
     let answer = 0;
     let sum = 0;
-    for (i=0;i<arr.length;i++){
+    for(i=0;i<arr.length;i++){
         sum += arr[i]
     }
     answer = sum / arr.length
