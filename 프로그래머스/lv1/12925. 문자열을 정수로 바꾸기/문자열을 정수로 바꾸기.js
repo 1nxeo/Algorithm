1f867c561d4f5d2c4b76480f55f2bed4d1207bba
Inseo Yang <124547871+1nxeo@users.noreply.github.com>
2023-02-16 23:05:50 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.js
+++ 프로그래머스/lv1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.js
@@ -1,17 +1,5 @@
 function solution(s) {
     let answer = 0;
-
-    // for(i=0;i<s.length;i++){
-    //     if(typeof s[0] == 'number'){
-    //         answer = parseInt(s)
-    //     }else{
-    //         answer = parseInt(s[0])
-    //         for(i=1;i<s.length;i++){
-    //         answer += parseInt(i)
-    //         }
-    //     }
-    // }
     answer = parseInt(s)
-    
     return answer;
 }
