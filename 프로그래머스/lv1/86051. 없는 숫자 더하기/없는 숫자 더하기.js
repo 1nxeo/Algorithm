2646a465f16d4d8916b6a537756703e26a82e1d6
Inseo Yang <124547871+1nxeo@users.noreply.github.com>
2023-02-16 23:13:33 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,12 +1,14 @@
 function solution(numbers) {
-    let answer = 0;
+    let answer = -1;
     let sum = 45
-    let list = [1,2,3,4,5,6,7,8,9]
-    for (i=0;i<numbers.length;i++){
-        if(list.includes(numbers[i]) ){
-            sum -= numbers[i]
+    let list = [0,1,2,3,4,5,6,7,8,9]
+    for(i=0;i<numbers.length;i++){
+        if(numbers[i] in list){
+                sum = sum - numbers[i]
+                answer = sum
+            }
         }
-    }
-    answer = sum
+        
+    
     return answer;
 }
