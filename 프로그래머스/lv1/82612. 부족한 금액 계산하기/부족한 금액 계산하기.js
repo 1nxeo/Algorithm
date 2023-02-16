b23fff3937ee3aa4f29f929be4c8ff1739b3afbc
Inseo Yang <124547871+1nxeo@users.noreply.github.com>
2023-02-16 23:55:18 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -1,11 +1,12 @@
 function solution(price, money, count) {
     let answer = -1;
-    //이용료 합 count*(count+1)/2*price
-    let sum = count*(count+1)/2*price
-    if (sum>money){
-        answer = sum - money
+    let total = count*(count+1)/2*price
+    
+    if(total>money){
+        answer = total - money
     }else{
         answer = 0
     }
+
     return answer;
 }
