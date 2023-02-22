f7248d449a885af3abffe00f20a6c289789bc00e
Inseo Yang <124547871+1nxeo@users.noreply.github.com>
2023-02-22 22:52:51 +0900

    [level 1] Title: 하샤드 수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
@@ -1,12 +1,14 @@
 function solution(x) {
     let answer = true;
     // 자릿수의 합%x == 0
-    let sum = 0;
+    // let sum = 0;
     let list = String(x).split('')
     
-    for(i=0;i<list.length;i++){
-        sum += Number(list[i]) 
-    }
+    let sum = String(x).split('').reduce((a, b, i)=> Number(b)+a,0)
+    
+    // for(i=0;i<list.length;i++){
+    //     sum += Number(list[i]) 
+    // }
     
     if(x%sum !== 0){
         answer = false
