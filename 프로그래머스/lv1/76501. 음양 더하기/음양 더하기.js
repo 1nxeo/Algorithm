df8b40032973590cb3ad7bdac585c3ca942e2dd0
Inseo Yang <124547871+1nxeo@users.noreply.github.com>
2023-02-22 23:18:24 +0900

    [level 1] Title: 음양 더하기, Time: 0.19 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -1,15 +1,17 @@
 function solution(absolutes, signs) {
     let answer = 123456789;
     let sum = 0;
-
-    for(i=0;i<absolutes.length;i++){
-        console.log(signs[i])
-        if(signs[i] == false ){
-            sum -= absolutes[i]
-        }else{
-            sum += absolutes[i]
-        }
-}
-    answer = sum
+    
+    answer = absolutes.reduce((acc, cur, index)=> acc + (signs[index]?+cur:-cur), 0)
+    
+//     for(i=0;i<absolutes.length;i++){
+//         console.log(signs[i])
+//         if(signs[i] == false ){
+//             sum -= absolutes[i]
+//         }else{
+//             sum += absolutes[i]
+//         }
+// }
+//     answer = sum
     return answer;
 }
