8f3ef2365b240c485829e5231649c80e17f63077
Inseo Yang <124547871+1nxeo@users.noreply.github.com>
2023-03-11 10:07:12 +0900

    [level 0] Title: 가까운 수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -0,0 +1,5 @@
+function solution(array, n) {
+    var minnum = array.sort().map((item)=> Math.abs(item-n));
+    let minasnwerindex =minnum.indexOf(Math.min(...minnum))
+return array[minasnwerindex];
+}
