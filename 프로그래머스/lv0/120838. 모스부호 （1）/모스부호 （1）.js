5e9c1829a1603c2e8527b3c101d258f03d2224b4
Inseo Yang <124547871+1nxeo@users.noreply.github.com>
2023-05-06 02:04:00 +0900

    [level 0] Title: 모스부호 (1), Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,14 @@
+function solution(letter) {
+    let answer = '';
+    const letterArr = letter.split(" ")
+    const morse = {
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+    }
+    answer= letterArr.map((item)=> morse[item]).join("")
+
+    return answer;
+}
