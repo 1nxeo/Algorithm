9fb59cc9a4a603c560048581d80c4558cfe84eea
Inseo Yang <124547871+1nxeo@users.noreply.github.com>
2023-06-14 11:06:19 +0900

    Update 피보나치 수.js

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
@@ -5,6 +5,9 @@ function solution(n) {
     fib[i] = (fib[i - 1] + fib[i - 2]) % 1234567;
   }
 
-  const answer = fib[n];
-  return answer;
+  return fib[n];
 }
+
+
+// 동적 계획법 사용
+// 이미 계산한 값을 배열에 저장해두고, 그 다음 숫자를 계산할 때 새로 연산할 필요 없이 저장된 값을 쓰는 것
