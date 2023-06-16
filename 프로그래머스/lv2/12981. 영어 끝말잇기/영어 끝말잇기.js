a195a8c8d8891903e81bd194e216a3d42374444a
Inseo Yang <124547871+1nxeo@users.noreply.github.com>
2023-06-16 21:18:18 +0900

    Update 영어 끝말잇기.js

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -6,8 +6,7 @@ function solution(n, words) {
 
   for (let i = 1; i < wordsLength; i++) {
     let firstChar = words[i][0];
-    let beforeWord = words[i - 1].length;
-    let lastChar = words[i - 1][beforeWord - 1];
+    let lastChar = words[i - 1][words[i - 1].length - 1];
 
     if (firstChar !== lastChar || words.slice(0, i).includes(words[i])) {
       player = (i % n) + 1;
