31d15e23d6137001d692f5891af43aa00f0a6b24
Inseo Yang <124547871+1nxeo@users.noreply.github.com>
2023-11-24 11:35:38 +0900

    [level 2] Title: 타겟 넘버, Time: 1.26 ms, Memory: 35.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,36 +1,19 @@
 function solution(numbers, target) {
-    let count = 0; // 방법의 수를 카운트하는 변수
+    let answer = 0;
     
     const dfs = (start, sum) => {
         if(start === numbers.length){
             if(sum === target){
-                count++
+                answer++
             }
+            
             return;
         }
         
-        dfs(start + 1, sum + numbers[start]);
+        dfs(start + 1, sum + numbers[start])
         dfs(start + 1, sum - numbers[start])
     }
     
-//     // 재귀적으로 탐색하며 타겟 넘버를 만들 수 있는 경우의 수를 계산
-//     function dfs(index, sum) {
-//         // 배열의 끝까지 모든 숫자를 확인한 경우
-//         if (index === numbers.length) {
-//             // 타겟 넘버를 만들면 count 증가
-//             if (sum === target) {
-//                 count++;
-//             }
-//             return;
-//         }
-
-//         // 현재 숫자를 더하거나 빼는 경우를 모두 탐색
-//         dfs(index + 1, sum + numbers[index]); // 더하기
-//         dfs(index + 1, sum - numbers[index]); // 빼기
-//     }
-
-    // 초기 index가 0이고 합계가 0부터 시작
-    dfs(0, 0);
-
-    return count;
+    dfs(0,0)
+    return answer;
 }
