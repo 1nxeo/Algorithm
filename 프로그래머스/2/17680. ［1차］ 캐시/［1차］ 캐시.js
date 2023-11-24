7978f68946a3eb3fac8a075a91184349320cc433
Inseo Yang <124547871+1nxeo@users.noreply.github.com>
2023-11-24 09:38:39 +0900

    [level 2] Title: [1차] 캐시, Time: 0.74 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -1,27 +1,30 @@
 function solution(cacheSize, cities) {
-    let cache = new Map();
-    let time = 0;
-
-    if (cacheSize === 0) {
-        return cities.length * 5; // 캐시 크기가 0일 경우 모든 도시를 캐시 미스로 처리하여 5초씩 걸림
-    }
-
-    cities.forEach(city => {
-        city = city.toLowerCase();
-
-        if (cache.has(city)) {
-            cache.delete(city);
-            cache.set(city, true);
-            time += 1; // 캐시 히트 시간은 1초
-        } else {
-            if (cache.size >= cacheSize) {
-                const leastRecentlyUsed = [...cache.keys()][0]; // LRU 캐시에서 가장 오래된 항목 추출
-                cache.delete(leastRecentlyUsed);
+    let answer = 0;
+    const hit = 1;
+    const miss = 5;
+    const cached = new Map();
+    if(cacheSize === 0) return 5 * cities.length
+    
+    cities.forEach(item => {
+        const city = item.toUpperCase();
+        
+        if(cached.has(city)){
+            cached.delete(city)
+            cached.set(city, true)
+            answer += hit
+        }else{
+            if(cached.size >= cacheSize){
+                const target = Array.from(cached.keys())[0]
+                cached.delete(target)
             }
-            cache.set(city, true);
-            time += 5; // 캐시 미스 시간은 5초
+            cached.set(city, true)
+            answer += miss
         }
-    });
-
-    return time;
+        
+        
+    })
+    
+    
+    
+    return answer;
 }
